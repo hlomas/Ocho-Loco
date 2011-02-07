e78358610ca68979863790a85c287183bbda0d48
hlomas <khenryscorpio@gmail.com>
2011-02-06 21:51:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OchoLoco::Application.config.secret_token = '23f1f31be318abf1272342ed71c9f946602fb6bfce1d0f313ad6cafd0ab4fd550bd55a00404845b1cf5d9695c049bbc917d003a50357f306d7efc8b58aab80c5'
